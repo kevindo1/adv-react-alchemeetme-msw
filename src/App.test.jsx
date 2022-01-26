7216ca7d8c806e173b2fd523a11681d73ac4035a
Kevin Do <kevind0501@gmail.com>
2022-01-26 15:38:48 -0800

    passing both tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,14 +4,16 @@
 import { screen, render } from '@testing-library/react'
 // 🚨
 // import rest
+import { rest } from 'msw'
 // import setupServer
+import { setupServer } from 'msw/node'
 import App from './App'
 
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -19,13 +21,31 @@ const user = {
   color: 'crimson',
 }
 
+// use this API URL: https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users
+/**
+ * Goes inside the callback function body
+ *    const select = req.url.searchParams.get('select')
+ * if (select) === '*') {
+ *  return res()
+ * }
+ */
+
 // 🚨 Create your server
+const server = setupServer(
+  rest.get(`https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users`, (req, res, ctx) => {
+    const select = req.url.searchParams.get('select')
+    if (select === '*') {
+      return res(ctx.json([user]))
+    }
+    return res(ctx.status(500))
+  })
+)
 
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen(server))
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.listen())
 
 test('Should render the header', async () => {
   render(<App />)
@@ -53,7 +73,15 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
   }
 
   // 🚨 Use the server to change the response for this test
-
+  server.use(
+    rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users', (req, res, ctx) => {
+      const select = req.url.searchParams.get('select')
+      if (select === '*') {
+        return res(ctx.json([sasuke]))
+      }
+      return res(ctx.status(500))
+    })
+  )
   render(<App />)
 
   const profileName = await screen.findByText(sasuke.name)
